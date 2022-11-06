ef23f147268ea8a43ed0a832b8d974582ca3bd18
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-11-06 12:56:54 -0500

    Call to slider function.

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -1,8 +1,10 @@
 import htmlHead from "./htmlHead1.js";
 import header from "./header1.js";
+import slider from "./slider.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const $header = document.querySelector("header");
   htmlHead();
   if ($header) header("header");
+  slider();
 });
