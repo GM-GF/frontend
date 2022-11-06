7daf0219d207f9ad36430d9cb9caa3b5574f6275
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-11-06 15:13:12 -0500

    Adding only the functions that the index.html needs.

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -1,10 +1,7 @@
-import htmlHead from "./htmlHead1.js";
-import header from "./header1.js";
 import slider from "./slider.js";
+import video from "./video.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const $header = document.querySelector("header");
-  htmlHead();
-  if ($header) header("header");
   slider();
+  video("video[data-video]");
 });
