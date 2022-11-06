bc9a11e5f889d082bef2bdc1df4c29854dc47b6d
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-11-06 13:59:49 -0500

    Call of the lazy loading functionality.

--- src/js/mainPages/index.js
+++ src/js/mainPages/index.js
@@ -1,7 +1,9 @@
 import imagesResponsive from "./images-responsive.js";
+import lazyLoad from "./lazy-loading.js";
 import scrollAnimation from "./scrollAnimation.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   imagesResponsive();
   scrollAnimation(".animate");
+  lazyLoad(".lazy-loading img");
 });
