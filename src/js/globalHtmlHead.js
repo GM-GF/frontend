70a08b92dbbd27609c5fdf3b923369a399f249a1
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-11-06 13:30:42 -0500

    Adding global head tags.

---
+++ src/js/globalHtmlHead.js
@@ -0,0 +1,15 @@
+document.querySelector("head").insertAdjacentHTML(
+  "beforeend",
+  `
+    <!-- Global styles -->
+    <link rel="stylesheet" href="./src/css/global.css" />
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="https://quiwop.github.io/media/favicon.png" />
+    <!-- Font Family -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+  `
+);
