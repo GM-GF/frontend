1c8c22083db05aa7267c266f7e49a83325963f3f
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-11-07 13:50:03 -0500

    Detecting when the page is ready to remove the banner from the user's view.

--- src/js/mainPages/index.js
+++ src/js/mainPages/index.js
@@ -1,16 +1,29 @@
 import footer from "./footer1.js";
 import header from "./header1.js";
-import htmlHead from "./htmlHead1.js";
 import imagesResponsive from "./images-responsive.js";
-import lazyLoad from "./lazy-loading.js";
 import scrollAnimation from "./scrollAnimation.js";
+import lazyLoad from "./lazy-loading.js";
+
+const $body = document.querySelector("body"),
+  $containerLoader = document.getElementById("banner");
+$body.classList.add("disable-scroll");
+let interval = setInterval(() => {
+  if (document.readyState == "complete") {
+    clearInterval(interval);
+    setTimeout(() => {
+      $body.classList.remove("disable-scroll");
+      $containerLoader.classList.add("banner-hide");
+      scrollTo({ top: 0 });
+      setTimeout(() => $body.removeChild($containerLoader), 200);
+    }, 1000);
+  }
+}, 500);
 
 document.addEventListener("DOMContentLoaded", () => {
   const $header = document.querySelector("header");
   const $footer = document.querySelector("footer");
   if ($header) header("header");
   if ($footer) footer();
-  htmlHead();
   imagesResponsive();
   scrollAnimation(".animate");
   lazyLoad(".lazy-loading img");
