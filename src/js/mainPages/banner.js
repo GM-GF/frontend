78a4674b20ec0fd2acbeb38155c3e3f46523e9af
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-11-07 13:57:23 -0500

    Replacing SVG code with an img tag.

--- src/js/mainPages/banner.js
+++ src/js/mainPages/banner.js
@@ -8,17 +8,9 @@ document.body.insertAdjacentHTML(
       </div>
       <div class="banner-loader">
         <div class="banner-loader-container">
-          <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="margin: auto; background: none; display: block; shape-rendering: auto;" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-            <circle cx="50" cy="50" r="0" fill="none" stroke="#ffffff" stroke-width="4">
-              <animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="0s"/>
-              <animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="0s"/>
-            </circle><circle cx="50" cy="50" r="0" fill="none" stroke="#ffffff" stroke-width="4">
-              <animate attributeName="r" repeatCount="indefinite" dur="1s" values="0;40" keyTimes="0;1" keySplines="0 0.2 0.8 1" calcMode="spline" begin="-0.5s"/>
-              <animate attributeName="opacity" repeatCount="indefinite" dur="1s" values="1;0" keyTimes="0;1" keySplines="0.2 0 0.8 1" calcMode="spline" begin="-0.5s"/>
-            </circle>
-          </svg>
+          <img src="https://g-m-g-f.github.io/images/assets/banner/loader.svg" alt="Banner o cargador." />
         </div>
       </div>
     </section>
-    `
+  `
 );
