11e483f22578a2b9230c39b1016ce5141070aed4
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-11-07 09:18:59 -0500

    Change file to path src/js/mainPages/htmlHead1.js

--- src/js/index/htmlHead1.js
+++
@@ -1,9 +0,0 @@
-export default function htmlHead() {
-  document.querySelector("head").insertAdjacentHTML(
-    "beforeend",
-    `
-      <!-- Styles -->
-      <link rel="stylesheet" href="./src/css/header1.css" />
-    `
-  );
-}
