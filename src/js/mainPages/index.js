acb6c1a0cfe1946df46cabe5fb52044cf9df8cd2
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-11-07 09:23:45 -0500

    Global functions for main pages.

--- src/js/mainPages/index.js
+++ src/js/mainPages/index.js
@@ -1,8 +1,16 @@
+import footer from "./footer1.js";
+import header from "./header1.js";
+import htmlHead from "./htmlHead1.js";
 import imagesResponsive from "./images-responsive.js";
 import lazyLoad from "./lazy-loading.js";
 import scrollAnimation from "./scrollAnimation.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const $header = document.querySelector("header");
+  const $footer = document.querySelector("footer");
+  if ($header) header("header");
+  if ($footer) footer();
+  htmlHead();
   imagesResponsive();
   scrollAnimation(".animate");
   lazyLoad(".lazy-loading img");
