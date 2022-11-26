a6a22b22ebf251d1574604ebcc16cdb450e90579
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-11-26 14:49:16 -0500

    Animate.css integrating only in the main or informative pages. src/js/globalHtmlHead.js src/js/mainPages/htmlHead1.js

--- src/js/globalHtmlHead.js
+++ src/js/globalHtmlHead.js
@@ -9,7 +9,5 @@ document.querySelector("head").insertAdjacentHTML(
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Baloo+Thambi+2:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-    <!-- Animate.css -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
   `
 );
--- src/js/mainPages/htmlHead1.js
+++ src/js/mainPages/htmlHead1.js
@@ -5,5 +5,7 @@ document.querySelector("head").insertAdjacentHTML(
     <link rel="stylesheet" href="./src/css/banner.css" />
     <link rel="stylesheet" href="./src/css/header1.css" />
     <link rel="stylesheet" href="./src/css/footer1.css" />
+    <!-- Animate.css -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
   `
 );
