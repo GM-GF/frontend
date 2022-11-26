534a3e5d82ecd0bf584fadf6e0d10c1a515fa2cd
David Santiago Avellaneda Montero <davellaneda.ingeniero@gmail.com>
2022-11-26 14:43:48 -0500

    Integrating global css for login pages.

--- src/js/loginPages/htmlHead2.js
+++ src/js/loginPages/htmlHead2.js
@@ -3,6 +3,7 @@ document.querySelector("head").insertAdjacentHTML(
   `
     <!-- Styles -->
     <link rel="stylesheet" href="./src/css/header2.css" />
+    <link rel="stylesheet" href="./src/css/loginPages.css" />
     <link rel="stylesheet" href="./src/css/alerts.css" />
   `
 );
